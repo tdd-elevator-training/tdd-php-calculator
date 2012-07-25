81540c648eebdec1587407e71a71bcb36bec7f3e
apofig <apofig@gmail.com>
2012-07-25 22:10:32 +0300

    Перая зеленая полоса. Моделька умеет делать 2*2 :) а так же запускается веб проект с hello world.

---
+++ application/models/Calculator.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class Calculator {
+
+	public function __construct() {
+
+	}
+	
+	public function calculate($expression) {
+		return '4';	
+	}
+}
+
+?>
---
+++ tests/application/models/CalculatorTest.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once 'PHPUnit\Framework\TestCase.php';
+require_once 'application\models\Calculator.php';
+
+class CalculatorTest extends PHPUnit_Framework_TestCase {
+	
+	private $Calculator;
+	
+	protected function setUp() {
+		parent::setUp ();
+		$this->Calculator = new Calculator();
+	
+	}
+	
+	protected function tearDown() {
+		$this->Calculator = null;		
+		parent::tearDown ();
+	}
+	
+	public function __construct() {
+	}
+	
+	public function testShould4When2Plus2() {
+ 		$actual =  $this->Calculator->calculate('2+2');
+ 		$this->assertEquals('4', $actual);
+	}
+
+}
+?>
