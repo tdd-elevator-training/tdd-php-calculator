34d2f34c5af5152131944bcd220498624083761e
apofig <apofig@gmail.com>
2012-08-07 13:02:33 +0300

    Упс, это избыточно

--- application/models/Calculator10.php
+++ application/models/Calculator10.php
@@ -4,7 +4,7 @@
 class Calculator10 {	
 	
 	public function calculate($expression) {
-		preg_match_all("/[0-9A-F]+/", $expression, $out);
+		preg_match_all("/[0-9]+/", $expression, $out);
 		
 		$sum = (int)($out[0][0]) + 
 			   (int)($out[0][1]);					
