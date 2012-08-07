e6519c4ac5af2e56a701969506dc1563c10c9d73
apofig <apofig@gmail.com>
2012-08-07 12:56:50 +0300

    Hефакторинг - выделил setup в тесте

--- tests/application/models/CalculatorTest.php
+++ tests/application/models/CalculatorTest.php
@@ -5,15 +5,26 @@ require_once 'application\models\Calculator.php';
 
 class CalculatorTest extends PHPUnit_Framework_TestCase {
 		
+	private $Calculator;
+	
+	protected function setUp() {
+		parent::setUp ();
+		$this->Calculator = new Calculator();
+	
+	}
+	
+	protected function tearDown() {
+		$this->Calculator = null;
+		parent::tearDown ();
+	}
+	
 	public function testShould4When2Plus2() {
-		$calculator = new Calculator();
-		$actual =  $calculator->calculate('2+2');
+		$actual =  $this->Calculator->calculate('2+2');
  		$this->assertEquals('4', $actual);
 	}	
 	
 	public function testShould7When3Plus4() {
-		$calculator = new Calculator();
-		$actual =  $calculator->calculate('3+4');
+		$actual =  $this->Calculator->calculate('3+4');
 		$this->assertEquals('7', $actual);
 	}
 	
