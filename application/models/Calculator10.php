4d00910b0565d381e3f6fd8ff2f0a883037e007f
apofig <apofig@gmail.com>
2012-08-07 13:39:34 +0300

    Исправил в чистовике калькулятор 10-чный

--- application/models/Calculator10.php
+++ application/models/Calculator10.php
@@ -6,10 +6,14 @@ class Calculator10 {
 	public function calculate($expression) {
 		preg_match_all("/[0-9]+/", $expression, $out);
 		
-		$sum = (int)($out[0][0]) + 
-			   (int)($out[0][1]);					
+		if (count($out[0]) < 2 || substr_count($expression, '+') != 1) {
+			throw new RuntimeException('Invalid expression format');
+		}
 		
-		return (string)($sum);	
+		$sum = (int)($out[0][0]) + 
+			   (int)($out[0][1]);	
+		
+		return $sum;
 	}
 }
 
--- tests/application/models/Calculator10Test.php
+++ tests/application/models/Calculator10Test.php
@@ -18,54 +18,35 @@ class Calculator10Test extends PHPUnit_Framework_TestCase {
 		parent::tearDown ();
 	}
 	
-	public function __construct() {
+	public static function provider() {
+		return array(
+				array(2, 2, 4),
+				array(3, 4, 7),
+				array(11, 22, 33),
+		);
 	}
 	
-	public function testShould4When2Plus2() {
- 		$actual =  $this->Calculator->calculate('2+2');
- 		$this->assertEquals('4', $actual);
+	/**
+	 * @dataProvider provider
+	 */
+	public function testShouldSumWhenXPlusY($x, $y, $expected) {
+		$actual =  $this->Calculator->calculate($x.'+'.$y);
+		$this->assertEquals($expected, $actual);
 	}
 	
-	public function testShould7When3Plus4() {
-		$actual =  $this->Calculator->calculate('3+4');
-		$this->assertEquals('7', $actual);
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testShouldExceptionWhenInvalidExpression() {
+		$actual =  $this->Calculator->calculate('1');
 	}
 
-	public function testShould10When9Plus1() {
-		$actual =  $this->Calculator->calculate('9+1');
-		$this->assertEquals('10', $actual);
-	}
-	
-	public function testShould13When11Plus2() {
-		$actual =  $this->Calculator->calculate('11+2');
-		$this->assertEquals('13', $actual);
-	}
-	
-	public function testShould22When11Plus11() {
-		$actual =  $this->Calculator->calculate('11+11');
-		$this->assertEquals('22', $actual);
-	}
-	
-	public function testShould44When22Plus22() {
-		$actual =  $this->Calculator->calculate('22+22');
-		$this->assertEquals('44', $actual);
-	}
-	
-	public function testShould198When99Plus99() {
-		$actual =  $this->Calculator->calculate('99+99');
-		$this->assertEquals('198', $actual);
-	}
-	
-	public function testShould101When100Plus1() {
-		$actual =  $this->Calculator->calculate('100+1');
-		$this->assertEquals('101', $actual);
-	}
-
-  	public function testShould_1111111110_When_123456789_Plus_987654321() {
-		$actual =  $this->Calculator->calculate('123456789+987654321');
-		$this->assertEquals('1111111110', $actual);
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testShouldExceptionWhenMoreThanOnePlus() {
+		$actual =  $this->Calculator->calculate('1++3');
 	}	
-	
 	
 }
 ?>
