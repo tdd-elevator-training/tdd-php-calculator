03098bd968ec789580ec7c413dfd466fdb7eb918
apofig <apofig@gmail.com>
2012-08-20 20:08:09 +0300

    Удалил из калькулятора зависимость полностью

--- application/models/Calculator.php
+++ application/models/Calculator.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once 'application\models\Convertor.php';
 
 class Calculator {	
 	
@@ -8,8 +9,9 @@ class Calculator {
 	
 	public function calculate($expression, $base) {
 		$this->Base = $base;
+		$convertor = new Convertor($base);
 
-		if ($this->isContainsInvalidNumber($expression)) {
+		if (!$convertor->isValid($expression)) {
 			throw new RuntimeException('Invalid number');
 		}
 		
@@ -23,54 +25,10 @@ class Calculator {
 			throw new RuntimeException('Invalid expression format');
 		}		
 			
-		$sum = $this->hexToInt($out[0][0]) + 
-			   $this->hexToInt($out[0][1]);	
+		$sum = $convertor->decode($out[0][0]) + 
+			   $convertor->decode($out[0][1]);	
 
-		return $this->intToHex ($sum);
-	}
-
-	private function isContainsInvalidNumber($expression) {
-		$is_invalid = false;
-		for ($index = 0; $index < strlen($expression); $index++) {
-			if ($expression[$index] == '+') {
-				continue;
-			}
-			
-			$int = $this->toInt($expression[$index]);
-			$is_invalid |= ($int === false) || $int >= $this->Base;				
-		}			
-		return $is_invalid;		
-	}
-	
-	private function intToHex($int) {
-		$result = '';
-		$low = $int;
-		do {
-			$high = $low % $this->Base;
-			$low = (int)$low / $this->Base;
-			$result = $this->toHex($high).$result;
-		} while ($low >= 1);
-		
-		return $result;
-	}
-
-	
-	private function toHex($int) {
-		return $this->Digits[$int];
-	}
-	
-	private function hexToInt($hex) {		
-		$sum = 0;
-		for ($index = 0; $index < strlen($hex); $index++) {
-			$sum = $this->Base*$sum + $this->toInt(substr($hex, $index, 1));
-		}
-		return $sum;
-
-	}
-	
-	private function toInt($hex) {
-		if (is_numeric($hex)) return $hex; 
-		return strpos($this->Digits, $hex);
+		return $convertor->code($sum);
 	}
 
 }
